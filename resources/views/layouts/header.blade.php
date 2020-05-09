0f3f730c6e52f8c1194e153deb78b654861b0661
unknown <angulo.aparicio.ismael@alumnat.copernic.cat>
2020-05-09 20:02:28 +0200

    plantilla del layout header creada

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestor Blogs</title>
+</head>
+<body>
+    <header id="header_gestor_blogs">
+        
+    </header>
+</body>
+</html>
